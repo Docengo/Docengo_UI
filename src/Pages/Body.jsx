211f50b7366ef95d26a859fb0748cb633521a402
iNitishSheoran <121354290+iNitishSheoran@users.noreply.github.com>
2025-07-05 18:05:46 +0530

    added banners

--- src/Pages/Body.jsx
+++ src/Pages/Body.jsx
@@ -1,13 +1,33 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
+import React, { useEffect, useState } from 'react';
+import Navbar from '../components/Navbar';
+import Banner1 from '../assets/Banner1.png';
+import Banner2 from '../assets/Banner2.png';
+
+const banners = [Banner1, Banner2];
 
 function Body() {
+  const [currentBanner, setCurrentBanner] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentBanner((prev) => (prev + 1) % banners.length);
+    }, 8000); // change every 5 seconds
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    
     <div>
-        <Navbar/>
+      <Navbar />
+      <div className="w-full">
+        <img
+          src={banners[currentBanner]}
+          alt={`Docengo Banner ${currentBanner + 1}`}
+          className="w-full h-auto max-h-[380px] mt-26 transition-opacity duration-1000"
+        />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Body
+export default Body;
