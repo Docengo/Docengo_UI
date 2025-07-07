cd5c116c43c360c9cdad8fdf959d5c2a2ea36c1c
Sonal Tyagi <sonaltyagi.biz@gmail.com>
2025-07-08 03:16:34 +0530

    Fixed Batches

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ import AllHelp from "./Pages/AllHelp"
 import AllFeedbacks from "./Pages/AllFeedbacks"
 import NeetBatches from "./components/NeetBatches";
 import JeeBatches from "./components/JeeBatches";
-import Batches from "./Pages/Batches";
+import Batches from "./Pages/BAtches";
 import RegistrationPayment from "./components/RegisterationPayment";
 import { Toaster } from "react-hot-toast";
 
