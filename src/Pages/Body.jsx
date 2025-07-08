67c76c08371351cace8387cf46171191300cea08
iNitishSheoran <121354290+iNitishSheoran@users.noreply.github.com>
2025-07-09 00:44:59 +0530

    Banner 4 updated

--- src/Pages/Body.jsx
+++ src/Pages/Body.jsx
@@ -13,9 +13,10 @@ import Highlights from '../components/Highlights';
 import FloatingFeedback from "../components/FloatingFeedback";
 import { Link } from 'react-router-dom';
 import Banner3 from '../assets/Banner3.png';
+import Banner4 from '../assets/Banner4.png';
 
 
-const banners = [Banner1, Banner2, Banner3];
+const banners = [Banner1, Banner2, Banner3, Banner4];
 
 function Body() {
   const [currentBanner, setCurrentBanner] = useState(0);
